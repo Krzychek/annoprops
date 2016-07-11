af9a8b2fd56deba6b0b20cc57e52692aa3a417a9
krzychek <krzysiek1993r@gmail.com>
2016-07-11 20:15:26 +0200

    move default serializers init to method

--- annoprops/src/main/java/org/annoprops/PropertyManagerBuilder.java
+++ annoprops/src/main/java/org/annoprops/PropertyManagerBuilder.java
@@ -3,60 +3,70 @@ package org.annoprops;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("WeakerAccess")
 public class PropertyManagerBuilder {
 
-    private final static Map<Class<?>, PropertySerializer> DEFAULT_SERIALIZERS = Collections.unmodifiableMap(new HashMap<Class<?>, PropertySerializer>() {{
-        SimplePropertySerializer simplePropertySerializer = new SimplePropertySerializer();
-        put(Integer.class, simplePropertySerializer);
-        put(int.class, simplePropertySerializer);
-        put(Double.class, simplePropertySerializer);
-        put(double.class, simplePropertySerializer);
-        put(Float.class, simplePropertySerializer);
-        put(float.class, simplePropertySerializer);
-        put(String.class, simplePropertySerializer);
-        put(Enum.class, simplePropertySerializer);
-    }});
-
     private final List<PropertyHolderProvider> propertyHolderProviders = new LinkedList<>();
-
     private final Collection<Object> propertyHolders = new ArrayList<>();
-
     private final Map<Class<?>, PropertySerializer> serializers = new HashMap<>();
 
+    private static Map<Class<?>, PropertySerializer> getDefaultSerializers() {
+        SimplePropertySerializer simplePropertySerializer = new SimplePropertySerializer();
+
+        return new HashMap<Class<?>, PropertySerializer>() {{
+            put(Integer.class, simplePropertySerializer);
+            put(int.class, simplePropertySerializer);
+            put(Double.class, simplePropertySerializer);
+            put(double.class, simplePropertySerializer);
+            put(Float.class, simplePropertySerializer);
+            put(float.class, simplePropertySerializer);
+            put(String.class, simplePropertySerializer);
+            put(Enum.class, simplePropertySerializer);
+        }};
+    }
+
+    @SuppressWarnings("unused")
     public PropertyManagerBuilder withSerializer(Class clazz, PropertySerializer serializer) {
         this.serializers.put(clazz, serializer);
         return this;
     }
 
+    @SuppressWarnings("unused")
     public PropertyManagerBuilder withSerializers(Map<Class<?>, PropertySerializer> serializers) {
         this.serializers.putAll(serializers);
         return this;
     }
 
+    @SuppressWarnings("unused")
     public PropertyManagerBuilder withDefaultSerializers() {
-        this.serializers.putAll(DEFAULT_SERIALIZERS);
+        this.serializers.putAll(getDefaultSerializers());
         return this;
     }
 
+    @SuppressWarnings("unused")
     public PropertyManagerBuilder withObject(Object propertyHolder) {
         propertyHolders.add(propertyHolder);
         return this;
     }
 
+    @SuppressWarnings("unused")
     public PropertyManagerBuilder withObjects(Collection<?> propertyHolder) {
         propertyHolders.addAll(propertyHolder);
         return this;
     }
 
+    @SuppressWarnings("unused")
     public PropertyManagerBuilder usingPropertyHolderProvider(PropertyHolderProvider propertyHolderProvider) {
         propertyHolderProviders.add(propertyHolderProvider);
         return this;
     }
 
+    @SuppressWarnings("unused")
     public PropertyManagerBuilder withObjects(Object... propertyHolder) {
         return withObjects(Arrays.asList(propertyHolder));
     }
 
+    @SuppressWarnings("unused")
     public PropertyManager build() {
         propertyHolderProviders.stream()
                 .map(PropertyHolderProvider::getPropertyHolders)
