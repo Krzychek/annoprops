989e3398bb7c4b8fdb14eb3f0f54d519086c9f93
krzychek <krzysiek1993r@gmail.com>
2016-07-12 12:23:43 +0200

    add factory methods to helper bean

--- annoprops/src/main/java/org/annoprops/PropertyManagerHelperBean.java
+++ annoprops/src/main/java/org/annoprops/PropertyManagerHelperBean.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
 
 
 /**
@@ -18,6 +20,22 @@ public class PropertyManagerHelperBean {
     public PropertyManagerHelperBean(PropertyManagerBuilder propertyManagerBuilder, File propertiesFile) {
         this.propertyManagerBuilder = propertyManagerBuilder;
         this.propertiesFile = propertiesFile;
+    }
+
+    @SuppressWarnings("unused")
+    public static PropertyManagerHelperBean createWithFileAndObjects(File propertiesFile, Collection<?> objects) {
+        return new PropertyManagerHelperBean(
+                PropertyManager.builder().withObjects(objects),
+                propertiesFile);
+    }
+
+    @SuppressWarnings("unused")
+    public static PropertyManagerHelperBean createWithFileAndObjects(File propertiesFile, Collection<?> objects, Map<Class<?>, PropertySerializer> serializers) {
+        return new PropertyManagerHelperBean(
+                PropertyManager.builder()
+                        .withObjects(objects)
+                        .withSerializers(serializers),
+                propertiesFile);
     }
 
     public PropertyManager getPropertyManager() {
